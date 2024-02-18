f980ecb008ead4b513b59607f4971662cec80ee0
KyanPepper <kyankks1@gmail.com>
2024-02-17 16:24:17 -0800

    tests for swagger and flask init

--- Backend/tests/test_routes.py
+++ Backend/tests/test_routes.py
@@ -1,5 +1,23 @@
 import os
 import pytest
+from app import app
 
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+        
+
+def test_Flask(client):
+    response = client.get('/test123')
+    assert response.status_code == 200
+    assert response.get_json() == {'message': 'Hello, World!'}
+
+def test_Swagger(client):
+    response = client.get('/test')
+    assert response.status_code == 200
+    assert response.get_data(as_text=True) == '"Hello, World!"\n'
 
 
