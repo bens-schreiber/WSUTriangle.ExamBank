66109c06d66ae887dad00bc7613b606a148dfaff
KyanPepper <kyankks1@gmail.com>
2024-02-17 18:19:56 -0800

    TBA

--- Backend/app/routes.py
+++ Backend/app/routes.py
@@ -5,7 +5,7 @@ from pymongo.collection import Collection
 from app import api
 
 
-@api.route("/swaggertest", methods=["GET", "POST"])
+@api.route("/swagger_test", methods=["GET", "POST"])
 class Swaggertest(Resource):
     def get(self):
         return "Hello, World!"
@@ -14,14 +14,13 @@ class Swaggertest(Resource):
         return "WORLD HELLO!"
 
 
-@app.route("/flasktest", methods=["GET"])
+@app.route("/flask_test", methods=["GET"])
 def flasktest():
     return jsonify({"message": "Hello, World!"})
 
 
 @app.route("/exam", methods=["POST"])
 def create_exam():
-    if request.method == "POST":
         data = request.json
         exam_name = data.get("exam_name")
         tags = data.get("tags", [])
@@ -34,18 +33,5 @@ def create_exam():
             return jsonify({"message": "Exam data inserted successfully"}), 201
         except Exception as e:
             return jsonify({"error": str(e)}), 500
+        
 
-
-@app.route("/exam", methods=["GET"])
-def search_exams():
-    if request.method == "GET":
-        exam_name = request.args.get("exam_name")
-        tags = request.args.getlist("tags")
-        query = {}
-        if exam_name:
-            query["exam_name"] = exam_name
-        if tags:
-            query["tags"] = {"$in": tags}
-        exams = db["exams"].find(query, {"_id": 0})
-        return jsonify(list(exams)), 200
-    return jsonify({"error": "Invalid request method"}), 405
--- Backend/tests/tests.py
+++ Backend/tests/tests.py
@@ -1,13 +1,16 @@
 import os
+from pymongo import MongoClient
 import pytest
 from app import app
 
+BLOB_IMAGE = 'https://wsutriangle.blob.core.windows.net/exam-bank/Triangle Logo.jpeg'
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
+    
         
 
 def test_flask(client):
@@ -19,6 +22,27 @@ def test_swagger(client):
     response = client.get('/swaggertest')
     assert response.status_code == 200
     assert response.get_data(as_text=True) == '"Hello, World!"\n'
+
+def test_Mongo(client):
+    client = MongoClient("mongodb://root:example@localhost:27017/")
+    db = client["test_database"]
+    collection = db["test_collection"]
+    collection.insert_one({"test_key": "test_value"})
+    result = collection.find_one({"test_key": "test_value"})
+    assert result is not None
+
+
+def test_exam_post(client):
+    post = client.post(
+        "/exam",
+        data={
+            "name": 'CALC3'
+        },
+    )
+    assert response.status_code == 200
+    assert response.get_data(as_text=True) == '"WORLD HELLO!"\n'
+
+def test_examget(client):  
 
 
 
